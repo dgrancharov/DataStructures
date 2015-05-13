81fd312d96a376a919158e53e0f5027cf49cbf9a
dgrancharov <damyan.grancharov@gmail.com>
2015-05-13 23:20:22 +0300

    task 1

---
+++ SortArrayOfNumbers/SortArrayOfNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortArrayOfNumbers
+{
+	class MainClass
+	{
+		public static void Main ()
+		{
+			char[] separators = new char[] {' '};
+			List<int> numbers = Console.ReadLine ().Split (separators, StringSplitOptions.RemoveEmptyEntries)
+				.Select(x => int.Parse(x)).ToList();
+
+			numbers.Sort ();
+
+			foreach (int number in numbers) 
+			{
+				Console.Write ("{0} ",number);
+			}
+			Console.WriteLine ();
+		}
+	}
+}
